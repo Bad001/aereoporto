089d3a74d42ed4235fb481f33d8851e1b39dc82e
Luca Rosi <rosiluca2001@gmail.com>
2023-02-01 03:04:28 +0100

    Fixato alcuni bug, introduzione al semaforo per la pista di atterraggio

--- aereoporto.c
+++ aereoporto.c
@@ -9,8 +9,9 @@
 #include <fcntl.h>
 #include <errno.h>
 #include <signal.h>
+#include <semaphore.h>
 
-#define NUM_AEREI 10
+#define NUM_AEREI 4
 
 struct aereo {
 	pid_t id;	// PID processo aereo
@@ -21,20 +22,21 @@ struct aereo {
 const char* getTime();
 int getRandomSec(int, int);
 
-// Signals
-
-/*void inserisciInCoda(pid_t);
-void rimuoviDallaCoda();
-void gestioneSegnali(int);
-*/
 int main() {
 	struct aereo aerei[NUM_AEREI];
-	int tempoPreparazione = 0, coda[NUM_AEREI], canale_torre_hangar[2], fileDescriptor, status;	// canale_torre_hangar[0] = read;[1] = write
+	sem_t pista;
+	int tempoPreparazione = 0, tempoDecollo = 0, coda[NUM_AEREI], fileDescriptor, status, autorizzazione;
 	pid_t hangar, torre;	// Dichiarazioni variabili di tipo pid_t per i PID dei nostri processi
+	if(mkfifo("canaleComunicazione", 0777) == -1) {	// Pipe named per il dialogo tra i vari processi
+		if(errno != EEXIST) {	// Se la pipe non è stata creata lo comunica all'utente e va in errore
+			fprintf(stderr, "Errore nella creazione del canale di comunicazione!\n");
+			return -1;
+		}
+	}
 	printf("\n  TEMPO   |   PID   | EVENTO\n");
 	if((hangar = fork()) < 0) {	// Se la nuova fork torna errore
 		fprintf(stderr, "Errore nella creazione del processo Hangar!\n");
-		return 1;
+		return -1;
 	}
 	else if(hangar == 0) {	// Processo figlio Hangar
 		hangar = getpid();	// Salvo il PID nella variabile hangar di tipo pid_t
@@ -43,58 +45,64 @@ int main() {
 			sleep(2);	// Per la creazione di un aereo ci vogliono 2 secondi
 			if((aerei[i].id = fork()) < 0) {	// Creo effettivamente l'aereo e ritorno eventuali errori
 				fprintf(stderr, "Errore nella creazione del processo Aereo %d!\n", i+1);
-				return 2;
+				return -1;
 			}
-			else if(aerei[i].id == 0) {	// Processi aerei
+			else if(aerei[i].id == 0) {	// Processi nipoti aerei
 				aerei[i].nome = i;
 				printf("%s|  \033[1;33m%d\033[0m   | Aereo %d creato dall'hangar\n", getTime(), getpid(), i+1);
 				tempoPreparazione = getRandomSec(3, 8);
 				printf("%s|  \033[1;33m%d\033[0m   | Aereo %d si sta preparando... tempo stimato %d secondi\n", getTime(), getpid(), i+1, tempoPreparazione);
 				sleep(tempoPreparazione);
 				printf("%s|  \033[1;33m%d\033[0m   | Aereo %d invia richiesta di decollo alla torre\n", getTime(), getpid(), i+1);
-				if((fileDescriptor = open("canale_torre_aereo", O_RDWR)) == -1) { // L'aereo richiede la disponibilità della pista alla torre
-					fprintf(stderr, "Errore apertura del file \"canale_torre_aereo\"!\n");
-					return 6;
+				// L'aereo ha finito di prepararsi e ora invia la richiesta di decollo alla torre
+				if((fileDescriptor = open("canaleComunicazione", O_RDWR)) == -1) { // L'aereo richiede la disponibilità della pista alla torre
+					fprintf(stderr, "Errore apertura del file \"canaleComunicazione\"!\n");
+					return -1;
 				}
 				if(write(fileDescriptor,&aerei[i].nome, sizeof(pid_t)) == -1) {
-					fprintf(stderr, "Errore scrittura nel file \"canale_torre_aereo\"!\n");
-					return 7;
+					fprintf(stderr, "Errore scrittura nel file \"canaleComunicazione\"!\n");
+					return -1;
 				}
-				waitpid(torre,&status,0);
+				//if(read(fileDescriptor, &autorizzazione, sizeof(pid_t)) == -1) {
+				//	fprintf(stderr, "Errore lettura del file \"canaleComunicazione\"!\n");
+				//	return -1;
+				//}
+				/*if (autorizzazione) {
+
+				}*/
 				close(fileDescriptor);
 				return 0;
 			}
-			if(i == (NUM_AEREI - 1)) {
-				printf("%s|  \e[1;91m%d\033[0m   | Hangar ha finito la creazione dei aerei e sta in attesa\n", getTime(), getpid());
-			}
+
 		}
-		for(int i = 0; i < 10; i++) {	// Hangar attende che tutti i processi figli aereo terminino
+		for(int i = 0; i < NUM_AEREI; i++) {	// Hangar attende che tutti i processi figli aereo terminino
 			waitpid(aerei[i].id,&status,0);
 		}
-		// OOO Qua va la notifica alla torre che gli aerei sono terminati dopodiché hangar termina (con una pipe unnamed)
+		printf("%s|  \e[1;91m%d\033[0m   | Hangar ha finito la creazione dei aerei e sta in attesa\n", getTime(), getpid());
+		// OOO Qua va la notifica alla torre che gli aerei sono terminati dopodiché hangar termina
+		return 0;
 	}
 	else {	// Processo padre Torre
 		torre = getpid();
+		//sem_init(&pista, 1, 2);
 		printf("%s|  \033[1;31m%d\033[0m   | Torre di controllo avviata\n", getTime(), getpid());
-		if(mkfifo("canale_torre_aereo", 0777) == -1) {	// Pipe named per dialogare con processi nipoti
-			if(errno != EEXIST) {	// Se il file non è stato creato lo comunica all'utente e va in errore
-				fprintf(stderr, "Errore nella creazione del canale di comunicazione Torre <--> Aerei!\n");
-				return 3;
-			}
-		}
-		if((fileDescriptor = open("canale_torre_aereo", O_RDWR)) == -1) {
-			fprintf(stderr, "Errore apertura del file \"canale_torre_aereo\"!\n");
-			return 4;
+		if((fileDescriptor = open("canaleComunicazione", O_RDWR)) == -1) {
+			fprintf(stderr, "Errore apertura del file \"canaleComunicazione\"!\n");
+			return -1;
 		}
 		for(int i = 0; i < NUM_AEREI; i++) {
 			if(read(fileDescriptor, &coda[i], sizeof(pid_t)) == -1) {
-				fprintf(stderr, "Errore lettura del file \"canale_torre_aereo\"!\n");
-				return 5;
+				fprintf(stderr, "Errore lettura del file \"canaleComunicazione\"!\n");
+				return -1;
 			}
 			printf("%s|  \033[1;31m%d\033[0m   | Torre ha ricevuto la richiesta di decollo dall'aereo %d\n", getTime(), getpid(), coda[i]+1);
+			//sem_post(&pista);
 		}
 		close(fileDescriptor);
-		waitpid(hangar,&status,0);
+		wait(NULL);
+		//sem_destroy(&pista);
+		unlink("canaleComunicazione");
+		return 0;
 	}
     return 0;
 }
